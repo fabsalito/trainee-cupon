19240f6b98e74f4db38352e0da58b571f5dcf3a5
fabricio <fabricio.salinas@gmail.com>
2012-04-01 11:36:41 -0500

    Subida a nuevo repositorio

--- src/Cupon/TiendaBundle/Controller/ExtranetController.php
+++ src/Cupon/TiendaBundle/Controller/ExtranetController.php
@@ -3,7 +3,7 @@
 namespace Cupon\TiendaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\Security\Core\SecurityContext;
 
 class ExtranetController extends Controller
 {
@@ -14,6 +14,21 @@ class ExtranetController extends Controller
     
     public function loginAction()
     {
+        // obtiene la petición
+        $peticion = $this->getRequest();
+        
+        // obtiene sesión
+        $sesion = $peticion->getSession();
+        
+        // obtiene error
+        $error = $peticion->attributes->get(
+            SecurityContext::AUTHENTICATION_ERROR,
+            $sesion->get(SecurityContext::AUTHENTICATION_ERROR)
+        );
 
+        // retorna respuesta
+        return $this->render('TiendaBundle:Extranet:login.html.twig', array(
+            'error' => $error
+        ));
     }
 }
--- src/Cupon/UsuarioBundle/Listener/LoginListener.php
+++ src/Cupon/UsuarioBundle/Listener/LoginListener.php
@@ -1,4 +1,5 @@
 <?php
+
 // src/Cupon/UsuarioBundle/Listener/LoginListener.php
 
 namespace Cupon\UsuarioBundle\Listener;
@@ -7,34 +8,39 @@ use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Router;
+use Symfony\Component\Security\Core\SecurityContext;
 
-class LoginListener
-{
-    private $router;
+class LoginListener {
+
+    private $router = null;
     private $ciudad = null;
-    
+    private $contexto = null;
+
     // constructor
-    public function __construct(Router $router)
-    {
+    public function __construct(Router $router, SecurityContext $contexto) {
         $this->router = $router;
+        $this->contexto = $contexto;
     }
-    
+
     // obtiene y almacena la ciudad del usuario que hizo login
-    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
-    {
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event) {
         $token = $event->getAuthenticationToken();
         $this->ciudad = $token->getUser()->getCiudad()->getSlug();
     }
-    
+
     // redirecciona a la ciudad del usuario
-    public function onKernelResponse(FilterResponseEvent $event)
-    {
+    public function onKernelResponse(FilterResponseEvent $event) {
         if (null != $this->ciudad) {
-            $portada = $this->router->generate('portada', array(
-                'ciudad' => $this->ciudad
-            ));
-            
+            if ($this->contexto->isGranted('ROLE_TIENDA')) {
+                $portada = $this->router->generate('extranet_portada');
+            } else {
+                $portada = $this->router->generate('portada', array(
+                    'ciudad' => $this->ciudad
+                        ));
+            }
             $event->setResponse(new RedirectResponse($portada));
+            $event->stopPropagation();
         }
     }
+
 }
